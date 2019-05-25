ebab8c60a4f5fdd73a4ca3b2deec8688b990b02f
Thidashwe <46780233+Thidashwe@users.noreply.github.com>
2019-05-25 17:30:01 +0900

    Raw coding.

--- app/src/main/java/com/example/ais_siren/Verified.java
+++ app/src/main/java/com/example/ais_siren/Verified.java
@@ -1,22 +1,8 @@
 package com.example.ais_siren;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Button;
 
-import org.eclipse.paho.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mqttv3.IMqttActionListener;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-
-import java.io.UnsupportedEncodingException;
 
 public class Verified extends AppCompatActivity {
 
