b678a78ec1b9db2c2c71d1a40cb584fc68b40c23
Thidashwe <46780233+Thidashwe@users.noreply.github.com>
2019-05-25 17:28:55 +0900

    Raw coding.

--- app/src/main/java/com/example/ais_siren/Verified.java
+++ app/src/main/java/com/example/ais_siren/Verified.java
@@ -25,42 +25,5 @@ public class Verified extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_pick_success);
 
-        Button btn_verify = (Button) findViewById (R.id.btn_pick_up_verify);
-        String str_verify =  findViewById (R.id.pick_up_code).toString ();
-        int num_verfiy = Integer.parseInt (str_verify);
-
-        String clientId = MqttClient.generateClientId();
-        final MqttAndroidClient client =
-                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
-                        clientId);
-
-        try {
-            IMqttToken token = client.connect();
-            token.setActionCallback(new IMqttActionListener () {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    // We are connected
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    // Something went wrong e.g. connection timeout or firewall problems
-
-                }
-            });
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-
-        String topic = "foo/bar";
-        String payload = str_verify;
-        byte[] encodedPayload = new byte[0];
-        try {
-            encodedPayload = payload.getBytes("UTF-8");
-            MqttMessage message = new MqttMessage(encodedPayload);
-            client.publish(topic, message);
-        } catch (UnsupportedEncodingException | MqttException e) {
-            e.printStackTrace();
-        }
     }
 }
